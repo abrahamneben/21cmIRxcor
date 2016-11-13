554896abed7ce7d2741452bfe3f9250d79fe71e6
Abraham <abrahamn@mit.edu>
2016-11-13 15:30:09 -0500

    finish catalog xcor study

--- fg_xcor/cat_utils.py
+++ fg_xcor/cat_utils.py
@@ -85,25 +85,97 @@ def plot_catalog_boundary(plt,cat,col):
     ra0,ra1,dec0,dec1 = cat.min_ra,cat.max_ra,cat.min_dec,cat.max_dec
     plt.plot([ra0,ra1,ra1,ra0,ra0],[dec0,dec0,dec1,dec1,dec0],col+'-')
     
-def cat2img(cat,bound_cat,dtheta):
-    thetax = np.cos(cat.mean_dec*np.pi/180.)*cat.ra
-    thetay = cat.dec
+def cat2img(cat,bound_cat,dtheta,jymin=0,jymax=1.e9):
+    fluxcut = (cat.jy < jymax)&(cat.jy > jymin)
+    thetax_fluxcut = np.cos(cat.mean_dec*np.pi/180.)*cat.ra[fluxcut]
+    thetay_fluxcut = cat.dec[fluxcut]
+    jy_fluxcut = cat.jy[fluxcut]
 
     thetax0 = np.cos(bound_cat.mean_dec*np.pi/180.)*bound_cat.min_ra
     thetay0 = bound_cat.min_dec
 
     fov = np.min([bound_cat.max_dec-bound_cat.min_dec,np.cos(bound_cat.mean_dec*np.pi/180.)*(bound_cat.max_ra-bound_cat.min_ra)])
     n = int(fov/dtheta)
-    print(n)
 
     img = np.zeros((n,n))
     for xi in range(n):
         for yi in range(n):
-            inpixel = (thetax > thetax0+xi*dtheta)&(thetax < thetax0+(xi+1)*dtheta)&\
-                      (thetay > thetay0+yi*dtheta)&(thetay < thetay0+(yi+1)*dtheta)
-            img[xi,yi] = np.sum(cat.jy[inpixel])
+            inpixel = (thetax_fluxcut > thetax0+xi*dtheta)&(thetax_fluxcut < thetax0+(xi+1)*dtheta)&\
+                      (thetay_fluxcut > thetay0+yi*dtheta)&(thetay_fluxcut < thetay0+(yi+1)*dtheta)
+            img[xi,yi] = np.sum(jy_fluxcut[inpixel])
     
     return img
 
-def xspec
+def calc_xspec(img1,img2,dtheta_deg,nbins,lmax):
+    n,dang = img1.shape[0],dtheta_deg*np.pi/180.
+    
+    #hann2D,hann2Drms = make_hann(n)
+    lvals = np.fft.fftfreq(n)*2*np.pi/dang
+    lx,ly = np.meshgrid(lvals,lvals)
+    lmag  = np.sqrt(lx**2+ly**2)
 
+    img1_ft = np.fft.fft2((img1-img1.mean()))
+    img2_ft = np.fft.fft2((img2-img2.mean()))
+    
+    lbinedges = np.linspace(0,lmax,nbins+1)
+    lbincenters = .5*(lbinedges[0:nbins]+lbinedges[1:nbins+1])
+    
+    xspec_binned = np.zeros(nbins)
+    pspec1_binned = np.zeros(nbins)
+    pspec2_binned = np.zeros(nbins)
+    bin_counts = np.zeros(nbins)
+    for bini in range(nbins):
+        inbin = (lmag>lbinedges[bini])&(lmag<lbinedges[bini+1])
+        bin_counts[bini] = np.sum(inbin)
+
+        xspec_binned[bini] = np.real(np.mean(img1_ft[inbin]*np.conj(img2_ft[inbin])))
+        pspec1_binned[bini] = np.mean(np.abs(img1_ft[inbin])**2)
+        pspec2_binned[bini] = np.mean(np.abs(img2_ft[inbin])**2)
+    
+    pspec_norm = (dang**2)/(n**2)
+    return lbincenters,pspec1_binned*pspec_norm,pspec2_binned*pspec_norm,xspec_binned*pspec_norm,bin_counts
+
+def plot_spectra(plt,lbins,pspec1,pspec2,xspec,bin_counts,ir_mwa_jymin_max):
+    ir_jymin,ir_jymax,mwa_jymin,mwa_jymax = ir_mwa_jymin_max
+    
+    plt.figure(figsize=(20,4))
+    
+    plt.subplot(141)
+    plt.loglog(lbins,pspec1)
+    plt.xlim([np.min(lbins),np.max(lbins)])
+    plt.ylim([1.e-11,1.e-8])
+    plt.xlabel('\ell')
+    plt.ylabel('C_\ell')
+    plt.title("IR, (%1.4f < flux < %1.4f) jy"%(ir_jymin,ir_jymax))
+    
+    plt.subplot(142)
+    plt.loglog(lbins,pspec2)
+    plt.xlim([np.min(lbins),np.max(lbins)])
+    plt.ylim([1.e-12,1.e-6])
+    plt.xlabel('\ell')
+    plt.ylabel('C_\ell')
+    plt.title("MWA, (%1.4f < flux < %1.4f) jy"%(mwa_jymin,mwa_jymax))
+    
+    plt.subplot(143)
+    ispos = xspec>0
+    isneg = xspec<0
+    plt.loglog(lbins[ispos],xspec[ispos],'o')
+    plt.loglog(lbins[isneg],-xspec[isneg],'v')
+    plt.loglog(lbins,np.sqrt(pspec1*pspec2/bin_counts))
+    plt.xlim([np.min(lbins),np.max(lbins)])
+    plt.ylim([1.e-15,1.e-8])
+    plt.xlabel('\ell')
+    plt.ylabel('C_\ell')
+    plt.title('cross spectrum')
+    
+    plt.subplot(144)
+    xcor = xspec/np.sqrt(pspec1*pspec2)
+    plt.semilogx(lbins[ispos],xcor[ispos],'o')
+    plt.semilogx(lbins[isneg],-xcor[isneg],'v')
+    plt.ylim([0,1])
+    plt.xlim([np.min(lbins),np.max(lbins)])
+    plt.xlabel('\ell')
+    plt.ylabel('c_\ell')
+    plt.title('cross correlation')
+    
+    plt.tight_layout()
