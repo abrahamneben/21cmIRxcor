8686676e3395f38171f99195d55b7c0450f6f507
Abraham Neben <abraham.neben@icloud.com>
2017-01-25 21:41:30 +0000

    nicer xspec sensitivities

--- stack_whole_field_and_sextract/run_sextractor.py
+++ stack_whole_field_and_sextract/run_sextractor.py
@@ -1,10 +1,13 @@
-#!/Users/abrahamn/anaconda2/bin/python
+#!/home/abrahamn/anaconda2/bin/python
 
 from subprocess import call
 
-analysis_root = '/Volumes/abraham/xcor_data/analysis/ATLAS_mwa57639/whole_field/'
+#analysis_root = '/Volumes/abraham/xcor_data/analysis/ATLAS_mwa57639/whole_field/'
+analysis_root = '/home/abrahamn/xcor_data/analysis/ATLAS_mwa57639/whole_field/'
 analysis_name = 'whole_field_bgsub'
 
+print(analysis_root+analysis_name+'.fits')
+
 # run sextractor
-call(['sex',analysis_root+analysis_name+'.fits','-c','source_finding.sex',\
+call(['sextractor',analysis_root+analysis_name+'.fits','-c','source_finding.sex',\
 		  '-CATALOG_NAME',analysis_root+analysis_name+'.sex',])
